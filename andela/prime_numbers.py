40e3ae0058cc82976c64ea272c12cf6826bfaa8f
live-beta <wanjala.sam.s@gmail.com>
2016-12-01 12:29:58 +0300

    Tested for String,decimal and floating point inputs: Negative number Validation

--- andela/prime_numbers.py
+++ andela/prime_numbers.py
@@ -2,6 +2,9 @@
 import math
 
 def prime_gen(number):
+    if number < 0:
+        print "Negative number, not allowed"
+
     while True:
         primetest = True
         for x in range(2, int(math.sqrt(number) + 1)):
@@ -12,4 +15,4 @@ def prime_gen(number):
             print number
         number -= 1
 
-print prime_gen(20)
+print prime_gen(6)
--- andela/test_prime_numbers.py
+++ andela/test_prime_numbers.py
@@ -4,5 +4,15 @@ from prime_number import is_prime
 
 class TestCalculator(unittest.TestCase):
     def test_negative_numbers(self):
-        self.assertEqual(is_prime(-3),'Negatives not allowed')
-    
+        self.assertEqual(prime_gen(-3),'Negatives not allowed')
+
+    def test_range_numbers(self):
+        range_size= number -2
+        if range_size <= 0:
+            print "no range can be computed"
+    def test_string_inputs(self):
+        self.assertEqual(prime_gen(str),'String inputs not valid')
+    def test_decimal_inputs(self):
+        self.assertEqual(prime_gen(1.1),'Decimal inputs are not allowed')
+    def test_float_inputs(self):
+        self.assertEqual(prime_gen(0.112),'Floating point inputs not allowed')
