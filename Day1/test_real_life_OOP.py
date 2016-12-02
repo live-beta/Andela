53637bebc9e715548e9cea7ecceac3dc93645fdc
live-beta <wanjala.sam.s@gmail.com>
2016-12-02 14:37:38 +0300

    All Code (Labs and Exersies)

--- Day1/test_real_life_OOP.py
+++ Day1/test_real_life_OOP.py
@@ -3,4 +3,4 @@ import unittest
 from real_life_OOP import Student
 class test_real_life_OOP:
     def test_inputs(self):
-        self.assertEqual(__init__(None,'sder','wasdf','123',None,None),'Negatives not allowed')
+        self.assertEqual(__init__(None,'sder','wasdf','123',None,None))
