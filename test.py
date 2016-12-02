1ba9e63cfb187a16d20662dea209c9122f36c802
live-beta <wanjala.sam.s@gmail.com>
2016-12-03 01:58:26 +0300

    Code Submitted To Lab

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from prime_number import prime_gen
+class TestCalculator(unittest.TestCase):
+    def test_negative_numbers(self):
+        self.assertEqual(prime_gen(-3),'Negatives not allowed')
+
+    def test_range_numbers(self):
+        range_size= number -2
+        if range_size <= 0:
+            print "no range can be computed"
+    def test_string_inputs(self):
+        self.assertEqual(prime_gen(str),'String inputs not valid')
+    def test_decimal_inputs(self):
+        self.assertEqual(prime_gen(1.1),'Decimal inputs are not allowed')
+    def test_float_inputs(self):
+        self.assertEqual(prime_gen(0.112),'Floating point inputs not allowed')
+    def test_result(self):
+	    self.assertEqual(prime_gen(3), 12 ,'Number is not prime')
