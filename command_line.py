26e9565012003a5a9224ee9dde04b83c936f2614
live-beta <wanjala.sam.s@gmail.com>
2016-12-03 03:46:29 +0300

    Andela Lab Code

--- command_line.py
+++ command_line.py
@@ -2,7 +2,7 @@
 import sys
 import httplib
 
-#loading a http server ip
+#loading a http server ip with api datapoints
 http_server =sys.argv[1]
 #building the connection
 connection_1= httplib.HTTPConnection(http_server)
@@ -11,7 +11,7 @@ while True:
    command= raw_input('Enter command (E.g GET index.html)')
    command = command.split()
    if command[0]=='exit': #Exit prompt
-     break 
+     break
     #beginning Request Phase
     connection_1.request(command[0], command[1])
 
